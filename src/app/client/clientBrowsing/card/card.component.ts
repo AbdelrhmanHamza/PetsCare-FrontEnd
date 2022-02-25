bf0bde34676ef098132d9360d81692e778e48a87
doha-pro <doha.kassem97@gmail.com>
2022-02-25 22:50:49 +0200

    card ng ready

--- src/app/client/clientBrowsing/card/card.component.ts
+++ src/app/client/clientBrowsing/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Bussiness } from 'src/app/_models/service.model';
 
 @Component({
   selector: 'app-card',
@@ -9,7 +10,30 @@ export class CardComponent implements OnInit {
 
   constructor() { }
 
+  serviceCardInfos:Bussiness[]=
+  [
+    { id:1,
+      name:"Paul",
+      type :"Vet",
+      opensAt:"9:00",
+      closesAt:"5:00"},
+    { id:2,
+      name:"Paul",
+      type :"Vet",
+      opensAt:"9:00",
+      closesAt:"5:00"},
+    { id:3,
+      name:"Paul",
+      type :"Vet",
+      opensAt:"9:00",
+      closesAt:"5:00"}
+  ]
+
   ngOnInit(): void {
+  }
+
+  showDetails():void{
+    console.log("weeeeeeeeee")
   }
 
 }
