ab54074f3de6e8ae771f84115334f0de3fcd4b1a
doha-pro <doha.kassem97@gmail.com>
2022-03-01 00:25:50 +0200

    bussiness gallery - css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,7 @@ import { FormsModule } from '@angular/forms';
 import { BusinessRequestsTabComponent } from './Business/business-requests-tab/business-requests-tab.component';
 import { BusinessSubscriptionTabComponent } from './Business/business-subscription-tab/business-subscription-tab.component';
 import { MyBusinessesComponent } from './Business/my-businesses/my-businesses.component';
+import { BusinessGalleryPreviewComponent } from './Business/business-gallery-preview/business-gallery-preview.component';
 
 @NgModule({
   declarations: [
@@ -51,7 +52,8 @@ import { MyBusinessesComponent } from './Business/my-businesses/my-businesses.co
     CardComponent,
     PaginationComponent,
     ContainerComponent,
-    MyBusinessesComponent
+    MyBusinessesComponent,
+    BusinessGalleryPreviewComponent
   ],
   imports: [
     BrowserModule,
