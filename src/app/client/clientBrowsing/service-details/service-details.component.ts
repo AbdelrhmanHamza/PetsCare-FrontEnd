b74586b6d8f9091e50e96453b627e7fd30497deb
doha-pro <doha.kassem97@gmail.com>
2022-03-01 17:43:02 +0200

    when client browsing service details component without the transition of images

--- src/app/client/clientBrowsing/service-details/service-details.component.ts
+++ src/app/client/clientBrowsing/service-details/service-details.component.ts
@@ -8,6 +8,16 @@ import { Bussiness } from 'src/app/models/service.model';
 })
 export class ServiceDetailsComponent implements OnInit {
 serviceDetails:Bussiness={
+ 
+  name: "Doha",
+  type: "Vet",
+  opensAt: "always opened",
+  closesAt: "always closed :P ",
+  package_name: "Pack da7do7",
+  phone_number: "01000000",
+  package_description: "isn't it obvious ?? >(",
+  package_price: "msh el price nuber leh 3amleno string ",
+  address: "Hereeeeeeeeeeeeeeeeeeeeee"
   
 }
   constructor() { }
