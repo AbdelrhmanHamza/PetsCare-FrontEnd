23b7dd32bca059ff0bd4640ecd5f389cad8ce2b5
doha-pro <doha.kassem97@gmail.com>
2022-03-01 21:18:03 +0200

    complete the zoom in for pictures

--- src/app/client/clientBrowsing/service-details/service-details.component.ts
+++ src/app/client/clientBrowsing/service-details/service-details.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Bussiness } from 'src/app/models/service.model';
-
+export interface ImagesUrls{
+  src:string
+}
 @Component({
   selector: 'app-service-details',
   templateUrl: './service-details.component.html',
   styleUrls: ['./service-details.component.scss']
 })
 export class ServiceDetailsComponent implements OnInit {
+
+  sources:ImagesUrls[]=[
+    {src:"../../../../assets/img/team-1.jpg"},
+    {src:"../../../../assets/img/team-2.jpg"},
+    {src:"../../../../assets/img/blog-1.jpg"},
+    {src:"../../../../assets/img/blog-2.jpg"},
+ 
+  ]
 serviceDetails:Bussiness={
  
   name: "Doha",
