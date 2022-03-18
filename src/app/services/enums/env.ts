31a62d9df885adb6b339b10fb4304314099d7324
elzahraa <elzahraa.morsy.99@gmail.com>
2022-03-18 21:20:37 +0200

    frontend petcare

--- src/app/services/enums/env.ts
+++ src/app/services/enums/env.ts
@@ -1,4 +1,4 @@
-export const  AUTH_API = 'http://petscare.test/api/auth/';
+export const  AUTH_API = 'https://petcareproject.herokuapp.com/api/auth/';
 
-export const  REQ_API = 'http://petscare.test/api/';
+export const  REQ_API = 'https://petcareproject.herokuapp.com/api/';
 
