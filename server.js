1aaae506c76f714903ad12a59f00908b629d6a54
AmrSamy36 <amrabm36@gmail.com>
2022-03-29 01:22:43 +0200

    add file name to server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');  
 const app = express();  
-app.use(express.static(__dirname + '/dist'));  
+app.use(express.static(__dirname + '/dist/pet-care'));  
 app.all('*', (req, res) => {  
-  res.status(200).sendFile(__dirname + '/dist/index.html');  
+  res.status(200).sendFile(__dirname + '/dist/pet-care/index.html');  
 });  
 app.listen(process.env.PORT || 8080); 
