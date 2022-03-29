85e98858a3f1cc39eaad276fa6dbb41d8b8f0504
AmrSamy36 <amrabm36@gmail.com>
2022-03-29 03:08:36 +0200

    edit

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+      return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
 const express = require('express');
 const app = express();
+app.use(requireHTTPS);
+
 app.use(express.static('./dist/pet-care'));
 app.get('/*', function(req, res) {
   res.sendFile('index.html', {root: 'dist/pet-care/'}
